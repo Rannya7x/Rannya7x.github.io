4bdbb77f5bd471025497fdfe8358059640cd8245
Rannya7x <rannya7x@gmail.com>
2024-11-29 15:14:41 +0000

    Integração com servidor

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,10 +1,6 @@
-let { API_URL } = process.env;
-
-// Função para buscar os dados do endpoint
 export default async function fetchImages() {
   try {
-    const response = await fetch(process.env.API_URL); // Usando a URL importada
-    const data = await response.json();
+    const response = await fetch(process.env.API_URL); 
     return data;
   } catch (error) {
     console.error("Erro ao buscar dados:", error);
