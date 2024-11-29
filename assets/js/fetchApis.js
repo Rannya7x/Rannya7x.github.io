b476c7d4ab450d8d8846c58ca5fd20417e1b8460
Rannya7x <rannya7x@gmail.com>
2024-11-29 16:08:05 +0000

    url api

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,6 +1,6 @@
 export default async function fetchImages() {
   try {
-    const response = await fetch(process.env.API_URL); 
+    const response = await fetch("https://imersao-alura-backend.onrender.com/posts"); 
     const data = await response.json();
     return data;
   } catch (error) {
