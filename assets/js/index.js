0392276eba16f2eb16f9ccb5d5446d698670409f
Rannya7x <rannya7x@gmail.com>
2024-11-29 16:40:40 +0000

    mudança de endereço fetchApi

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-import fetchImages from "./fetchApis";
+import fetchImages from "assets/js/fetchApis.js";
 
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
