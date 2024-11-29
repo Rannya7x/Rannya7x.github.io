d594dfdcb9344021db9e16a26be2cbc1676247a9
Rannya7x <rannya7x@gmail.com>
2024-11-29 15:53:14 +0000

    refatoração

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,8 +1,10 @@
 export default async function fetchImages() {
   try {
     const response = await fetch(process.env.API_URL); 
+    const data = await response.json();
     return data;
   } catch (error) {
     console.error("Erro ao buscar dados:", error);
   }
+  
 }
