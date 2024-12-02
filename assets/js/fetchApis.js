1829206d04e7877286cc1078e8f035f948a0d4a2
Rannya7x <58481179+Rannya7x@users.noreply.github.com>
2024-12-02 15:39:21 +0000

    Update fetchApis.js

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,6 +1,7 @@
 export default async function fetchImages() {
   try {
-    const response = await fetch("https://imersao-alura-backend.onrender.com/posts"); 
+    const response = await fetch("https://imersao-alura-backend.onrender.com/posts");
+    console.log("response:", response);
     const data = await response.json();
     return data;
   } catch (error) {
